316b5b70276ac5678cb998e36c82cdd7baff9be5
osst1652 <oskar.steinhauf@gmail.com>
2017-01-18 11:06:04 +0100

    Lagt in alla filer i projektet.

--- ALDA_IN1/src/alda/linear/MyAldaList.java
+++ ALDA_IN1/src/alda/linear/MyAldaList.java
@@ -32,24 +32,5 @@ package alda.linear;
  */
 public class MyAldaList {
 	
-	public interface ALDAList<E> extends Iterable <E>{
-		public void add(E element);
-
-		public void add(int index, E element);
-
-		public E remove(int index);
-
-		public boolean remove(E element);
-
-		public E get(int index);
-
-		public boolean contains(E element);
-
-		public int indexOf(E element);
-
-		public void clear();
-
-		public int size();
-	}
 
 }
